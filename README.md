2f153440ae17efe965a71be0ac4f042974a85f13
Felipe <cvfelipeoliveira@gmail.com>
2020-09-05 22:57:34 -0400

    del

--- README.md
+++
@@ -1,1 +0,0 @@
-# smkt-batch
--- src/main/java/br/com/smktbatch/service/ParameterService.java
+++
@@ -1,9 +0,0 @@
-package br.com.smktbatch.service;
-
-import br.com.smktbatch.model.Parameter;
-
-public interface ParameterService {
-	
-	Parameter getParameterByClient(Long idClient);
-
-}
