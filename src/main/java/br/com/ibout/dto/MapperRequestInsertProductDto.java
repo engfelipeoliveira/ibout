c9b63b0b22208253cea19196e3e5be40c05ae572
ads14331 <ads14331@PPF8026.ssq.local>
2020-09-26 11:44:04 -0400

    mapping

--- src/main/java/br/com/ibout/dto/MapperRequestInsertProductDto.java
+++ src/main/java/br/com/ibout/dto/MapperRequestInsertProductDto.java
@@ -1,6 +1,10 @@
 package br.com.ibout.dto;
 
+import static java.lang.String.format;
+import static org.apache.commons.lang3.StringUtils.containsIgnoreCase;
+import static org.apache.commons.lang3.StringUtils.isBlank;
 import static org.apache.commons.lang3.StringUtils.replace;
+import static org.apache.commons.lang3.StringUtils.trimToEmpty;
 
 import br.com.ibout.model.local.Product;
 import lombok.Builder;
@@ -16,23 +20,32 @@ import lombok.ToString;
 @Builder(toBuilder = true)
 public class MapperRequestInsertProductDto {
 	
+	private static String getUnitFromProductDesc(Product product) {
+		if((!isBlank(product.getComplement()) && containsIgnoreCase(product.getComplement(), "KG")) ||
+				(!isBlank(product.getDescription()) && containsIgnoreCase(product.getDescription(), "KG"))) {
+			return "KG";
+		}else {
+			return "UN";			
+		}
+	}
+	
 	public static RequestInsertProductDto fromProductDto(Product product) {
 		return RequestInsertProductDto.builder()
-				.codigo(product.getCode())
-				.descricao(product.getDescription() != null ? product.getDescription() : " ")
-				.marca(product.getBrand())
-				.complemento(product.getComplement() != null ? product.getComplement() : " ")
-				.grupo(product.getGroupProduct())
-				.preco(product.getPrice() != null ? replace(product.getPrice(), ",", ".") : null)
-				.preco_oferta(product.getPriceSold() != null ? replace(product.getPriceSold(), ",", ".") : null)
-				.preco_clube(product.getPriceClub() != null ? replace(product.getPriceClub(), ",", ".") : null)
-				.oferta(0L) //.oferta(product.getSold())
-				.estoque(product.getStock())
-				.codigo_interno(product.getInternalCode())
-				.vasilha(product.getBowl())
+				.codigo(trimToEmpty(product.getCode()))
+				.descricao(trimToEmpty(product.getDescription()))
+				.marca(trimToEmpty(product.getBrand()))
+				.complemento(trimToEmpty(product.getComplement()))
+				.grupo(trimToEmpty(product.getGroupProduct()))
+				.preco(!isBlank(product.getPrice()) ? replace(product.getPrice(), ",", ".") : null)
+				.preco_oferta(!isBlank(product.getSold()) ? replace(product.getPriceSold(), ",", ".") : null)
+				.preco_clube(!isBlank(product.getPriceClub()) ? replace(product.getPriceClub(), ",", ".") : null)
+				.oferta(isBlank(product.getSold()) ? Long.parseLong(product.getSold()) : 0L)
+				.estoque(trimToEmpty(product.getStock()))
+				.codigo_interno(trimToEmpty(product.getInternalCode()))
+				.vasilha(trimToEmpty(product.getBowl()))
 				.id_estabelecimento(product.getIdClient())
-				.foto("img/" + product.getCode() + ".png")
-				.unidade("UN")
+				.foto(isBlank(product.getPhoto()) ? format("%s%s%s", "img/", product.getCode(), ".png") : product.getPhoto())
+				.unidade(isBlank(product.getUnit()) ? getUnitFromProductDesc(product) : product.getUnit())
 				.visivel(1L)
 				.build();
 	}
